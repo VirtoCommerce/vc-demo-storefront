cec9b9f077a46f8e66e15d0a6eac568228163ac0
Ilya Litvinenko <ilya.ivanov6@gmail.com>
2020-09-23 16:41:45 +0200

    VP-4961: Update order status if payment is successfull (#15)
    * VP-4961: Update order status if payment is successfull
    
    * VP-4961: Create class and string constants for all possible statuses

---
+++ VirtoCommerce.Storefront.Model/Order/OrderStatuses.cs
@@ -0,0 +1,11 @@
+
+namespace VirtoCommerce.Storefront.Model.Order
+{
+    public class OrderStatuses
+    {
+        public const string PaidOrderStatus = "Paid";
+        public const string CompletedOrderStatus = "Completed";
+        public const string NewOrderStatus = "Unpaid";
+
+    }
+}
