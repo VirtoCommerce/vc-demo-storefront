e1acc4ce4fd0aad42223c94fd5b68ab3b0c3ce47
trueboroda <superiorboroda@gmail.com>
2020-09-25 10:18:41 +0300

    VP-4688: Take into account PR notes
    Remove empty lines, Add comma

--- VirtoCommerce.Storefront.Model/Customer/Contact.cs
+++ VirtoCommerce.Storefront.Model/Customer/Contact.cs
@@ -17,40 +17,29 @@ namespace VirtoCommerce.Storefront.Model.Customer
         /// Returns the last name of the customer.
         /// </summary>
         public string LastName { get; set; }
-
         public string MiddleName { get; set; }
-
         public string Salutation { get; set; }
-
         public string PhotoUrl { get; set; }
-
         public string TimeZone { get; set; }
         public string DefaultLanguage { get; set; }
-
         public string Title { get; set; }
-
         public Address DefaultBillingAddress { get; set; }
         public Address DefaultShippingAddress { get; set; }
-
         public string OrganizationId { get; set; }
         public Organization Organization { get; set; }
         //TODO: It needs to be rework to support only a multiple  organizations for a customer by design.
         public IList<string> OrganizationsIds { get; set; } = new List<string>();
-
         /// <summary>
         /// Returns true if the customer accepts marketing, returns false if the customer does not.
         /// </summary>
         public bool AcceptsMarketing { get; set; }
-
         /// <summary>
         /// Returns the default customer_address.
         /// </summary>
         public Address DefaultAddress { get; set; }
-
         /// <summary>
         /// All contact security accounts
         /// </summary>
         public IEnumerable<SecurityAccount> SecurityAccounts { get; set; }
-
     }
 }
