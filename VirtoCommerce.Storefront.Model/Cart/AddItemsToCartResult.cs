3ab628766c9d8c954bf79b2b2fbfc4f443652461
trueboroda <superiorboroda@gmail.com>
2020-09-28 10:07:15 +0300

    VP-5110: Add API method for bulk products adding to cart

---
+++ VirtoCommerce.Storefront.Model/Cart/AddItemsToCartResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtoCommerce.Storefront.Model.Cart
+{
+    public class AddItemsToCartResult
+    {
+        public bool IsSuccess { get; set; }
+
+        public string ErrorCode { get; set; }
+    }
+}
