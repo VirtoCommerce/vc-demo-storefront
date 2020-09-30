b724ba66ead63748fab8fce7a1733b5b3f1b5412
Aleksandr Vishniakov <asvishnyakov@hotmail.com>
2020-09-30 16:41:58 +0200

    VP-5103: Update mock to newly demo data

--- VirtoCommerce.Storefront/Domain/Catalog/DemoCatalogService.cs
+++ VirtoCommerce.Storefront/Domain/Catalog/DemoCatalogService.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using VirtoCommerce.Storefront.AutoRestClients.CatalogModuleApi;
 using VirtoCommerce.Storefront.Infrastructure;
@@ -32,49 +34,54 @@ namespace VirtoCommerce.Storefront.Domain
 
         public async Task<ProductPart[]> GetProductPartsAsync(string productId)
         {
-            var products = await GetProductsAsync(new[]
+            var categories = new Dictionary<string, string>
             {
-                "baa4931161214690ad51c50787b1ed94",
-                "e9de38b73c424db19f319c9538184d03",
-                "ec235043d51848249e90ef170c371a1c",
-                "dae730451bc745bfa642870bdf22f150",
-                "5512e3a5201541769e1d81fc5217490c"
+                { "Cases", "f8457db1-6c48-48dd-9ff0-0d31f3db0d93" },
+                { "Motherboard", "db7752b9-4cf0-4df1-98c9-b5fa110439d1" },
+                { "CPU", "92adf559-70dd-4d39-a75e-4c9c0c7c4e05" },
+                { "Memory", "5554ca91-12b5-42df-bd36-7454683f05d5" },
+                { "GPU", "f49f255f-512b-4172-ba3c-cfaa8995c159" },
+            };
+            var searchResult = await SearchProductsAsync(new ProductSearchCriteria
+            {
+                Outline = "bea69328-eb20-4da9-ac3d-52e9045c18d2", PageSize = int.MaxValue
             });
+            var products = searchResult.Products;
             var parts = new[]
             {
                 new ProductPart
                 {
                     Name = "Case",
                     Image = new Image { Url = "https://raw.githubusercontent.com/VirtoCommerce/vc-demo-theme-b2b/dev/assets/images/mock/case.svg" },
-                    Items = products,
+                    Items = products.Where(x => x.CategoryId == categories["Cases"]).ToArray(),
                     SelectedItemId = "baa4931161214690ad51c50787b1ed94"
                 },
                 new ProductPart
                 {
                     Name = "Motherboard",
                     Image = new Image { Url = "https://raw.githubusercontent.com/VirtoCommerce/vc-demo-theme-b2b/dev/assets/images/mock/motherboard.svg" },
-                    Items = products,
+                    Items = products.Where(x => x.CategoryId == categories["Motherboard"]).ToArray(),
                     SelectedItemId = "e9de38b73c424db19f319c9538184d03"
                 },
                 new ProductPart
                 {
                     Name = "Processor",
                     Image = new Image { Url = "https://raw.githubusercontent.com/VirtoCommerce/vc-demo-theme-b2b/dev/assets/images/mock/processor.svg" },
-                    Items = products,
+                    Items = products.Where(x => x.CategoryId == categories["CPU"]).ToArray(),
                     SelectedItemId = "ec235043d51848249e90ef170c371a1c"
                 },
                 new ProductPart
                 {
                     Name = "Memory",
                     Image = new Image { Url = "https://raw.githubusercontent.com/VirtoCommerce/vc-demo-theme-b2b/dev/assets/images/mock/memory.svg" },
-                    Items = products,
+                    Items = products.Where(x => x.CategoryId == categories["Memory"]).ToArray(),
                     SelectedItemId = "dae730451bc745bfa642870bdf22f150"
                 },
                 new ProductPart
                 {
                     Name = "Graphics",
                     Image = new Image { Url = "https://raw.githubusercontent.com/VirtoCommerce/vc-demo-theme-b2b/dev/assets/images/mock/graphics.svg" },
-                    Items = products,
+                    Items = products.Where(x => x.CategoryId == categories["GPU"]).ToArray(),
                     SelectedItemId = "5512e3a5201541769e1d81fc5217490c"
                 }
             };
