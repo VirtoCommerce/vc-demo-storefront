c351986bdb14e40101ced9fe73d3e42a9d4db166
Anton Veretennikov <superiorboroda@gmail.com>
2021-02-01 10:42:01 +0300

    VDS-749: Save Name and ImageUrl in configured Group entities for cart and order (#54)
    * Regenerate Order and Cart modules API clients
    
    * Revert GetInvoicePdfWithHttpMessagesAsync method behavior customly
    
    * Extend configured group models with Name and ImageUrl
    
    * Save Name and ImageUrl props to configured group at adding
    
    * Revert Revert GetInvoicePdf continue
    
    * Remove second constructor

--- VirtoCommerce.Storefront.Model/Cart/Demo/ConfiguredGroup.cs
+++ VirtoCommerce.Storefront.Model/Cart/Demo/ConfiguredGroup.cs
@@ -33,6 +33,10 @@ namespace VirtoCommerce.Storefront.Model.Cart.Demo
 
         public string ProductId { get; set; }
 
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
         public Product Product { get; set; }
 
         public DateTime CreatedDate { get; set; }
--- VirtoCommerce.Storefront/Controllers/Api/ApiCartDemoController.cs
+++ VirtoCommerce.Storefront/Controllers/Api/ApiCartDemoController.cs
@@ -60,7 +60,12 @@ namespace VirtoCommerce.Storefront.Controllers.Api
 
                 if (configuredGroup == null)
                 {
-                    configuredGroup = new Model.Cart.Demo.ConfiguredGroup(firstItem.Quantity, currency, configuredProductId);
+                    var configuredProduct = (await _catalogService.GetProductsAsync(new string[] { configuredProductId }, Model.Catalog.ItemResponseGroup.ItemSmall)).FirstOrDefault();
+                    configuredGroup = new Model.Cart.Demo.ConfiguredGroup(firstItem.Quantity, currency, configuredProductId)
+                    {
+                        Name = configuredProduct?.Name ?? "",
+                        ImageUrl = configuredProduct?.PrimaryImage?.Url
+                    };
                     cart.ConfiguredGroups.Add(configuredGroup);
                 }
                 else
